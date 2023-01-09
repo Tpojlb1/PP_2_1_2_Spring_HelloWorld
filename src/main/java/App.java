0321b67a6dbe9998e5083b428160a8ca087a131b
Tpojlb1 <118221543+Tpojlb1@users.noreply.github.com>
2023-01-09 11:06:02 +0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,16 +2,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
-    public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
-        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
-        Cat catbean1 =(Cat) applicationContext.getBean("Cat");
-        Cat catbean2 =(Cat) applicationContext.getBean("Cat");
-        System.out.println(catbean1.getMessage());
-        System.out.println(bean==bean1);
-        System.out.println(catbean1==catbean2);
-    }
+public static void main(String[] args) {
+    ApplicationContext applicationContext =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+    HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+    HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+    System.out.println(bean.getMessage());
+    Cat catbean1 = (Cat) applicationContext.getBean("Cat");
+    Cat catbean2 = (Cat) applicationContext.getBean("Cat");
+    System.out.println(catbean1.getMessage());
+    System.out.println(bean == bean1);
+    System.out.println(catbean1 == catbean2);
+}
 }
